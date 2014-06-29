0757a5a9caa4282ea574fa9cf477976b5eb189c1
kpkhomeworkdevtools <kpkhomeworkdevtools@gmail.com>
2014-06-29 21:12:52 +0300

    Task 02

---
+++ Homework_12_Due_29.06.2014/Task 02/T4Template/TemplateApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Program.cs" company="TelerikAcademy.com">For educational purposes only</copyright>
+// <author>My name is Legion for we are many.</author>
+
+namespace TemplateApplication
+{
+    using System;
+    using T4Template;
+
+    /// <summary>Contains main program logic.</summary>
+    public class Program
+    {
+        /// <summary>Program execution starts here.</summary>        
+        public static void Main()
+        {
+            TestClass.HelloWorld();
+        }
+    }
+}
